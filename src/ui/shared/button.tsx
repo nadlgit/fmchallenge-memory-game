2a71efd724dffba54554ffd1f8f6c63eb7bbb131
Nadine <nadlgit@users.noreply.github.com>
2023-05-03 12:57:58 -0400

    refactor: Button component style

--- src/ui/shared/button.tsx
+++ src/ui/shared/button.tsx
@@ -2,16 +2,16 @@ import styles from './button.module.css';
 
 type ButtonProps = {
   variant: 'big' | 'primary' | 'secondary';
-  fullWidth?: boolean;
   label: string;
   onClick: () => void;
 };
 
-export const Button = ({ variant, fullWidth = false, label, onClick }: ButtonProps) => (
-  <button
-    className={`${styles.button} ${styles[variant]} ${fullWidth ? styles.fullwidth : ''}`}
-    onClick={onClick}
-  >
-    {label}
-  </button>
-);
+export const Button = ({ variant, label, onClick }: ButtonProps) => {
+  const classes = [styles.button];
+  variant !== 'primary' && classes.push(styles[variant]);
+  return (
+    <button className={classes.join(' ')} onClick={onClick}>
+      {label}
+    </button>
+  );
+};
