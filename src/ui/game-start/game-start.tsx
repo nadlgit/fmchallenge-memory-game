19422e88a16e4d6f47c5b30fc39c9d2d51dda366
Nadine <nadlgit@users.noreply.github.com>
2023-05-09 15:10:25 -0400

    feat: game start screen behavior

--- src/ui/game-start/game-start.tsx
+++ src/ui/game-start/game-start.tsx
@@ -1,32 +1,54 @@
-import { Button, Logo } from '@/ui/shared';
+import { useState } from 'react';
+import { type GridSize } from '@/core';
+import { Button, Logo, useGame } from '@/ui/shared';
 import { Setting } from './setting';
 import styles from './game-start.module.css';
 
-export const GameStart = () => (
-  <div className={styles.container}>
-    <header>
-      <Logo variant="home" />
-    </header>
-    <main className={styles.main}>
-      <Setting
-        title="Select Theme"
-        options={['Numbers', 'Icons']}
-        initialOption="Numbers"
-        onChange={() => null}
-      />
-      <Setting
-        title="Numbers of Players"
-        options={['1', '2', '3', '4']}
-        initialOption="1"
-        onChange={() => null}
-      />
-      <Setting
-        title="Grid Size"
-        options={['4x4', '6x6']}
-        initialOption="4x4"
-        onChange={() => null}
-      />
-      <Button label="Start Game" variant="big" onClick={() => null} />
-    </main>
-  </div>
-);
+const initialSettings = {
+  theme: 'Numbers',
+  nbPlayers: '1',
+  gridSize: '4x4' as GridSize,
+};
+
+export const GameStart = () => {
+  const { createGame } = useGame();
+  const [settings, setSettings] = useState(initialSettings);
+  return (
+    <div className={styles.container}>
+      <header>
+        <Logo variant="home" />
+      </header>
+      <main className={styles.main}>
+        <Setting
+          title="Select Theme"
+          options={['Numbers', 'Icons']}
+          initialOption={initialSettings.theme}
+          onChange={(option) => setSettings((s) => ({ ...s, theme: option }))}
+        />
+        <Setting
+          title="Numbers of Players"
+          options={['1', '2', '3', '4']}
+          initialOption={initialSettings.nbPlayers}
+          onChange={(option) => setSettings((s) => ({ ...s, nbPlayers: option }))}
+        />
+        <Setting
+          title="Grid Size"
+          options={['4x4', '6x6']}
+          initialOption={initialSettings.gridSize}
+          onChange={(option) => setSettings((s) => ({ ...s, gridSize: option as GridSize }))}
+        />
+        <Button
+          label="Start Game"
+          variant="big"
+          onClick={() =>
+            createGame(
+              settings.theme === 'Icons',
+              Number.parseInt(settings.nbPlayers),
+              settings.gridSize
+            )
+          }
+        />
+      </main>
+    </div>
+  );
+};
--- src/ui/shared/context/game-provider.tsx
+++ src/ui/shared/context/game-provider.tsx
@@ -1,15 +1,28 @@
-import { createContext, type PropsWithChildren, useEffect, useState } from 'react';
-import { type GameState, onStateUpdate } from '@/core';
+import { createContext, type PropsWithChildren, useCallback, useEffect, useState } from 'react';
+import { type GameState, type GridSize, onStateUpdate, startNewGame } from '@/core';
 
 type GameContextValue = {
   isSettingsScreen: boolean;
+  isIconTheme: boolean;
   isEndGame: GameState['isEndGame'];
+  createGame: (iconTheme: boolean, nbPlayers: number, gridSize: GridSize) => void;
 };
 export const GameContext = createContext<GameContextValue | undefined>(undefined);
 
 export const GameProvider = ({ children }: PropsWithChildren) => {
-  const [isSettingsScreen] = useState<GameContextValue['isSettingsScreen']>(true);
+  const [isSettingsScreen, setIsSettingsScreen] =
+    useState<GameContextValue['isSettingsScreen']>(true);
+  const [isIconTheme, setIsIconTheme] = useState<GameContextValue['isIconTheme']>(false);
   const [isEndGame, setIsEndGame] = useState<GameContextValue['isEndGame']>(false);
+
+  const createGame: GameContextValue['createGame'] = useCallback(
+    (iconTheme, nbPlayers, gridSize) => {
+      setIsSettingsScreen(false);
+      setIsIconTheme(iconTheme);
+      startNewGame({ gridSize, nbPlayers });
+    },
+    []
+  );
 
   useEffect(
     () =>
@@ -20,6 +33,8 @@ export const GameProvider = ({ children }: PropsWithChildren) => {
   );
 
   return (
-    <GameContext.Provider value={{ isSettingsScreen, isEndGame }}>{children}</GameContext.Provider>
+    <GameContext.Provider value={{ isSettingsScreen, isIconTheme, isEndGame, createGame }}>
+      {children}
+    </GameContext.Provider>
   );
 };
