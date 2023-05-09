9d97f21025fcbd22feb22b9d7e1e7361e487a92e
Nadine <nadlgit@users.noreply.github.com>
2023-05-08 21:53:27 -0400

    feat: restartGame use case

---
+++ src/core/restart-game.test.ts
@@ -0,0 +1,116 @@
+import { game } from './game';
+import { type GameState } from './game-state';
+import { restartGame } from './restart-game';
+
+describe('restartGame()', () => {
+  const initialState: GameState = {
+    grid: [
+      [
+        { value: '1', isTurnedOver: true },
+        { value: '1', isTurnedOver: true },
+        { value: '2', isTurnedOver: true },
+        { value: '2', isTurnedOver: true },
+      ],
+      [
+        { value: '3', isTurnedOver: true },
+        { value: '3', isTurnedOver: true },
+        { value: '4', isTurnedOver: true },
+        { value: '4', isTurnedOver: true },
+      ],
+      [
+        { value: '5', isTurnedOver: true },
+        { value: '5', isTurnedOver: true },
+        { value: '6', isTurnedOver: true },
+        { value: '6', isTurnedOver: true },
+      ],
+      [
+        { value: '7', isTurnedOver: true },
+        { value: '7', isTurnedOver: true },
+        { value: '8', isTurnedOver: true },
+        { value: '8', isTurnedOver: true },
+      ],
+    ],
+    playerPairs: [7, 1],
+    activePlayerIndex: 1,
+    moves: 9,
+  };
+
+  beforeEach(() => {
+    game.grid = initialState.grid.map((row) => row.map((item) => ({ ...item })));
+    game.playerPairs = [...initialState.playerPairs];
+    game.activePlayerIndex = initialState.activePlayerIndex;
+    game.moves = initialState.moves;
+  });
+
+  it('keeps grid size', () => {
+    const initialSideLength = initialState.grid.length;
+    restartGame();
+    expect(game.grid).toHaveLength(initialSideLength);
+    for (const row of game.grid) {
+      expect(row).toHaveLength(initialSideLength);
+    }
+  });
+
+  it('sets grid with item pairs', () => {
+    const initialPairsCount = (initialState.grid.length * initialState.grid.length) / 2;
+    restartGame();
+    const gridValues = game.grid.reduce((acc, curr) => {
+      curr.forEach(({ value }) => {
+        if (!acc[value]) {
+          acc[value] = 0;
+        }
+        acc[value]++;
+      });
+      return acc;
+    }, {} as Record<(typeof game.grid)[number][number]['value'], number>);
+    expect(Object.values(gridValues)).toIncludeSameMembers(new Array(initialPairsCount).fill(2));
+  });
+
+  it('sets grid items as not turned', () => {
+    restartGame();
+    for (const row of game.grid) {
+      for (const { isTurnedOver } of row) {
+        expect(isTurnedOver).toBeFalse();
+      }
+    }
+  });
+
+  it('sets new grid values', () => {
+    restartGame();
+    expect(
+      game.grid.some((row, r) =>
+        row.some(({ value }, c) => value !== initialState.grid[r][c].value)
+      )
+    ).toBeTrue();
+  });
+
+  it('keeps number of player pairs', () => {
+    const initialSideLengthSize = initialState.playerPairs.length;
+    restartGame();
+    expect(game.playerPairs).toHaveLength(initialSideLengthSize);
+  });
+
+  it('sets all player pairs to 0', () => {
+    restartGame();
+    for (const val of game.playerPairs) {
+      expect(val).toBe(0);
+    }
+  });
+
+  it('sets game active player to player 1', () => {
+    restartGame();
+    expect(game.activePlayerIndex).toBe(0);
+  });
+
+  it('sets game moves to 0', () => {
+    restartGame();
+    expect(game.moves).toBe(0);
+  });
+
+  it('triggers game update callback', () => {
+    const callback = vi.fn();
+    game.registerUpdateCallback(callback);
+    restartGame();
+    expect(callback).toHaveBeenCalled();
+  });
+});
---
+++ src/core/restart-game.ts
@@ -0,0 +1,26 @@
+import { game } from './game';
+
+export const restartGame = () => {
+  resetGrid();
+  resetPlayerPairs();
+  game.activePlayerIndex = 0;
+  game.moves = 0;
+  game.notifyUpdate();
+};
+
+const resetGrid = () => {
+  const items = game.grid
+    .flatMap((row) => row.map(({ value }) => ({ value, isTurnedOver: false })))
+    .sort(() => Math.random() - 0.5);
+  for (let r = 0; r < game.grid.length; r++) {
+    for (let c = 0; c < game.grid[0].length; c++) {
+      game.grid[r][c] = items[r * game.grid.length + c];
+    }
+  }
+};
+
+const resetPlayerPairs = () => {
+  for (let i = 0; i < game.playerPairs.length; i++) {
+    game.playerPairs[i] = 0;
+  }
+};
