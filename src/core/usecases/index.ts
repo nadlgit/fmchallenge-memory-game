e4ae1de999137c0d93445a714b96fede78e23389
Nadine <nadlgit@users.noreply.github.com>
2023-05-09 20:01:46 -0400

    refactor: moved files

--- src/core/usecases/index.ts
+++ src/core/usecases/index.ts
@@ -1,4 +1,1 @@
-export { makeMove } from './make-move';
-export { onStateUpdate } from './on-state-update';
-export { restartGame } from './restart-game';
-export { startNewGame, type GridSize } from './start-new-game';
+export * from './game';
