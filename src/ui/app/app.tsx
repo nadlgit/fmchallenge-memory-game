80e266e1238dc2f533c167521d6504b2a339193c
Nadine <nadlgit@users.noreply.github.com>
2023-05-10 11:15:14 -0400

    feat: challenge attribution

--- src/ui/app/app.tsx
+++ src/ui/app/app.tsx
@@ -2,14 +2,19 @@ import { GameEnd } from '@/ui/game-end';
 import { GameInProgress } from '@/ui/game-in-progress';
 import { GameStart } from '@/ui/game-start';
 import { GameProvider, useGame } from '@/ui/shared';
+import { ChallengeAttribution } from './challenge-attribution';
+import styles from './app.module.css';
 
-export default function App() {
-  return (
+export const App = () => (
+  <div className={styles.app}>
     <GameProvider>
       <AppContent />
     </GameProvider>
-  );
-}
+    <footer className={styles.attribution}>
+      <ChallengeAttribution />
+    </footer>
+  </div>
+);
 
 const AppContent = () => {
   const { isSettingsScreen, isEndGame } = useGame();
---
+++ src/ui/app/challenge-attribution.tsx
@@ -0,0 +1,13 @@
+const challengeLink = (
+  // eslint-disable-next-line react/jsx-no-target-blank
+  <a href="https://www.frontendmentor.io/challenges/memory-game-vse4WFPvM" target="_blank">
+    Frontend Mentor
+  </a>
+);
+const authorLink = <a href="https://github.com/nadlgit">Nadine</a>;
+
+export const ChallengeAttribution = () => (
+  <p>
+    Challenge by {challengeLink}. Coded by {authorLink}.
+  </p>
+);
